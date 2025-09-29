bc19bcd16282a7cf756838c36355b2c5e1a7d5a7
Ayu Setyaning Tyas <ayusetyaningtyas13@gmail.com>
2025-09-29 23:16:50 +0700

    Add files via upload

---
+++ soal1tugas1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     float angka1, angka2;
+    cout << "masukkan angka1 : ";
+    cin >> angka1;
+    cout << "masukkan angka2 : ";
+    cin >> angka2;
+    
+    //penjumlahan 
+    cout << "penjumlahan : " << angka1 + angka2 << endl;
+    //pengurangan 
+    cout << "pengurangan : " << angka1 - angka2 << endl;
+    //perkalian
+    cout << "perkalian : " << angka1 * angka2 << endl;
+    //pembagian
+    cout << "pembagian : " << angka1 / angka2 << endl;
+    
+    return 0;
+}
